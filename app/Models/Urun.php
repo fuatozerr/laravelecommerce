5778b5a509d330dfadde75c4be977d72dedf67e5
fuatozerr <fuatozerr23@gmail.com>
2018-08-16 16:49:00 +0300

    Urun modeline ilişkilendirmeler random değerler atıldı

---
+++ app/Models/Urun.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Urun extends Model
+{
+    use SoftDeletes;
+    protected $table="urun";
+
+    protected $fillable=['urun_adi','slug','aciklama','fiyat'];
+
+    protected $guarded=[];
+
+
+    public function kategoriler()
+    {
+        return $this->belongsToMany('App\Models\Kategori','kategori_urun');
+    }
+
+
+    public function detay(){
+
+        return $this->hasOne('App\Models\UrunDetay');
+    }
+}
---
+++ database/migrations/2018_08_15_133702_create_urun_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUrunTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urun', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug',200);
+            $table->string('urun_adi',170);
+            $table->text('aciklama');
+            $table->decimal('fiyat',6,3);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urun');
+    }
+}
