0c6bf5fb670f8be78ede439dde53f1d08189fc7d
fuatozerr <fuatozerr23@gmail.com>
2018-09-04 15:28:56 +0300

    Sepet Yapısı bitirilmek üzere

---
+++ app/Models/SepetUrun.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SepetUrun extends Model
+{
+    use SoftDeletes;
+
+    protected $table="sepet_urun";
+    protected $guarded=[]; //tüm kolonlara ekleme izni verir
+}
